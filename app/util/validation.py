84bc26b55b15f4144c635e23896c5fb17f1b26e3
Kacper-Grzyb <kgrzyb2860@gmail.com>
2025-05-29 08:08:50 +0200

    test: file validation testing

--- app/util/validation.py
+++ app/util/validation.py
@@ -20,6 +20,10 @@ def basic_file_validation(
         else:
             # Basic file validation
             allowed_cube_extensions = [".raw", ".bin"]
+            if not hasattr(hdr_file, "filename"):
+                raise InvalidFileFormatError(detail="The provided hdr file is invalid - no attribute 'filename'")
+            if not hasattr(cube_file, "filename"):
+                raise InvalidFileFormatError(detail="The provided cube file is invalid - no attribute 'filename'")
             hdr_extension = "." + hdr_file.filename.split(".")[-1].lower()
             cube_extension = "." + cube_file.filename.split(".")[-1].lower()
             if hdr_extension != ".hdr":
@@ -27,9 +31,9 @@ def basic_file_validation(
             if cube_extension not in allowed_cube_extensions:
                 raise InvalidFileFormatError(detail="Invalid data cube file extension. Only '.raw' and '.bin' extensions are supported")
             if not hasattr(hdr_file, "file"):
-                raise InvalidFileFormatError(detail="The provided hdr file has no attribute 'file'")
+                raise InvalidFileFormatError(detail="The provided hdr file is invalid - no attribute 'file'")
             if not hasattr(cube_file, "file"):
-                raise InvalidFileFormatError(detail="The provided cube file has no attribute 'file'")
+                raise InvalidFileFormatError(detail="The provided cube file is invalid - no attribute 'file'")
     except Exception as e:
         raise InvalidFileFormatError(detail=f"Error occurred while validating uploaded files. Exception: {e}")
 
